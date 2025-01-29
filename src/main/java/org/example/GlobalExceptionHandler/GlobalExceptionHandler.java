701c22b0e09c3fca02285c8179ae2296c5e31fe7
Stefano Micciche <miccichestefano20@gmail.com>
2025-01-29 12:20:37 +0100

    feat: (UserEntity / UserEnum): UserEntity finished

--- src/main/java/org/example/GlobalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/org/example/GlobalExceptionHandler/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package org.example.GlobalExceptionHandler;
 
 import org.example.Flight.Exceptions.FlightNotFoundException;
+import org.example.Flight.Exceptions.InvalidFlightException;
+import org.example.Flight.Exceptions.InvalidFlightSeatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 
 @RestController
@@ -23,5 +24,36 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(InvalidFlightException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleInvalidFlightException(InvalidFlightException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(InvalidFlightSeatException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleInvalidFlightSeatsException(InvalidFlightSeatException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred: " + ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
---
+++ src/main/java/org/example/User/UserEnum.java
@@ -0,0 +1,6 @@
+package org.example.User;
+
+public enum UserEnum {
+    ROLE_USER,
+    ROLE_ADMIN
+}
